80904a44a1ca1095d437caf4e5c2ca1aaaee7fc7
angelsol299 <angelsol299@hotmail.com>
2018-12-21 22:15:18 +0100

    postform component

---
+++ src/components/PostForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>posts</h1>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
