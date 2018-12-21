65a003a6d03c0e404777729b66e16c5da37ad40b
angelsol299 <angelsol299@hotmail.com>
2018-12-21 22:38:38 +0100

    adding providers

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
+import { Povider } from "react-redux";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <br />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <br />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
