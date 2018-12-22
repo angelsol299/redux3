c04277c15f87376527f20ec2e057e52b3fade087
angelsol299 <angelsol299@hotmail.com>
2018-12-22 23:39:45 +0100

    creating more actions

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { createPost } from "../actions/postActions";
 
 class PostForm extends Component {
   constructor(props) {
